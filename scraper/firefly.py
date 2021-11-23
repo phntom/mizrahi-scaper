4ca229a153b933920d2f44f6b42f88969113e520
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2021-11-24 00:19:44 +0200

    more improvments and fixes, firefly still wip

--- scraper/firefly.py
+++ scraper/firefly.py
@@ -9,6 +9,8 @@ from scraper.common import ScrapeResults, id_for_transaction
 
 log = logging.getLogger(__name__)
 
+CURRENCY_TO_ID = {}
+
 
 def api_call(request, params=None, method='GET',
              endpoint='http://localhost:5464', data=None):
@@ -23,23 +25,19 @@ def api_call(request, params=None, method='GET',
     return r.json()
 
 
-def paginated_data_call(request, asset='asset', method='GET',
+def paginated_data_call(request, method='GET',
                         endpoint='http://localhost:5464', **kwargs):
     page1 = api_call(request, {
         'page': '1',
-        'type': asset,
         **kwargs
     }, method, endpoint)
     yield from page1['data']
     for page in range(2, page1['meta']['pagination']['total_pages'] + 1):
-        yield from api_call(request, {
-            'page': str(page),
-            'type': asset,
-        }, method, endpoint)['data']
+        yield from api_call(request, {'page': page, **kwargs}, method, endpoint)['data']
 
 
 def account_get_all(endpoint: str):
-    yield from paginated_data_call('accounts', endpoint=endpoint)
+    yield from paginated_data_call('accounts', type='asset', endpoint=endpoint)
 
 
 def account_create(
@@ -69,39 +67,51 @@ def account_create(
         zoom_level: int = 0,
         endpoint: str = None,
 ):
+    data = {
+        'name': name,
+        'type': account_type,
+        'iban': iban,
+        'bic': bic,
+        'account_number': account_number,
+        'opening_balance': str(opening_balance) if opening_balance else '',
+        'opening_balance_date': opening_balance_date.strftime("%Y-%m-%d")
+        if opening_balance_date else '',
+        'virtual_balance': str(virtual_balance) if virtual_balance else '',
+        'currency_id': str(currency_id) if currency_id else '',
+        'currency_code': currency_code.upper(),
+        'active': str(active).lower(),
+        'order': order,
+        'include_net_worth': str(include_net_worth).lower(),
+        'account_role': account_role,
+        'credit_card_type': credit_card_type,
+        'monthly_payment_date': monthly_payment_date.strftime("%Y-%m-%d")
+        if monthly_payment_date else None,
+        'liability_type': liability_type,
+        'liability_direction': liability_direction,
+        'interest': str(interest),
+        'interest_period': interest_period,
+        'notes': notes,
+        'latitude': latitude,
+        'longitude': longitude,
+        'zoom_level': zoom_level,
+    }
+    cleanup = set()
+    for k, v in data.items():
+        if v is None:
+            cleanup.add(k)
+    for k in cleanup:
+        data.pop(k)
     return api_call(
         request='accounts',
         method='POST',
         endpoint=endpoint,
-        data={
-            'name': name,
-            'type': account_type,
-            'iban': iban,
-            'bic': bic,
-            'account_number': account_number,
-            'opening_balance': str(opening_balance) if opening_balance else '',
-            'opening_balance_date': opening_balance_date.strftime("%Y-%m-%d")
-            if opening_balance_date else '',
-            'virtual_balance': str(virtual_balance) if virtual_balance else '',
-            'currency_id': str(currency_id) if currency_id else '',
-            'currency_code': currency_code,
-            'active': active,
-            'order': order if order is not None else 1,
-            'include_net_worth': include_net_worth,
-            'account_role': account_role,
-            'credit_card_type': credit_card_type,
-            'monthly_payment_date': monthly_payment_date.strftime("%Y-%m-%d")
-            if monthly_payment_date else '',
-            'liability_type': liability_type,
-            'liability_direction': liability_direction,
-            'interest': str(interest) if interest else '',
-            'interest_period': interest_period,
-            'notes': notes,
-            'latitude': latitude,
-            'longitude': longitude,
-            'zoom_level': zoom_level,
-        }
+        data=data,
     )
+
+
+def currency_get_all(endpoint: str):
+    for currency in paginated_data_call('currencies', endpoint=endpoint):
+        CURRENCY_TO_ID[currency['attributes']['code']] = int(currency['id'])
 
 
 def transaction_get_all(
@@ -152,10 +162,10 @@ def transaction_create(
         method='POST',
         endpoint=endpoint,
         data={
-            "error_if_duplicate_hash": True,
-            "apply_rules": True,
-            "fire_webhooks": True,
-            "group_title": "Split transaction title ???",
+            "error_if_duplicate_hash": "true",
+            "apply_rules": "true",
+            "fire_webhooks": "true",
+            "group_title": f"{t_date} {t_type} of {amount}",
             "transactions": [
                 {
                     "type": t_type,
@@ -167,7 +177,7 @@ def transaction_create(
                     "source_name": t_source,
                     "destination_id": str(t_target_id),
                     "destination_name": t_target,
-                    "reconciled": True,
+                    "reconciled": "true",
                     "notes": note,
                     "external_id": external_id,
                     "process_date": (process_date.strftime("%Y-%m-%d") + "T00:00:00+02:00") if process_date else '',
@@ -218,6 +228,8 @@ def update_account(account_id, result, currency_t, endpoint):
 
 
 def firefly_upload(result: ScrapeResults, endpoint: str):
+    currency_get_all(endpoint)
+
     for account in account_get_all(endpoint):
         number = account['attributes'].get('account_number')
         cleanup = []
@@ -239,11 +251,22 @@ def firefly_upload(result: ScrapeResults, endpoint: str):
         account = account_create(
             name=f'{result.bank} {currency.upper()}',
             account_type='asset',
+            account_role='defaultAsset',
             account_number=number,
             opening_balance=opening_balance,
             opening_balance_date=opening_balance_date,
-            endpoint=endpoint
-        )
+            endpoint=endpoint,
+            active=True,
+            include_net_worth=True,
+            currency_code=currency,
+            currency_id=CURRENCY_TO_ID[currency.upper()],
+            credit_card_type='monthlyFull',
+            monthly_payment_date=date(2020, 1, 1),
+            liability_type='loan',
+            liability_direction='credit',
+            interest=0.0,
+            interest_period='monthly',
+        )['data']
         update_account(account['id'], result, currency, endpoint)
 
 
